6a422835fa068d2f50b014c29c5ea79a0328584c
Sergey Muratov <van.der.msa@gmail.com>
2019-05-24 12:33:34 +0300

    State after Chapter 22

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -1,5 +1,6 @@
 from WasRun import WasRun
 from TestCase import TestCase
+from TestResult import TestResult
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
@@ -17,6 +18,13 @@ class TestCaseTest(TestCase):
         result = test.run()
         assert("1 run, 1 failed" == result.summary())
         
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert("1 run, 1 failed" == result.summary())
+        
 TestCaseTest("testTemplateMethod").run()
 TestCaseTest("testResult").run()
+TestCaseTest("testFailedResultFormatting").run()
 TestCaseTest("testFailedResult").run()
