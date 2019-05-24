9ad69a1c94561eadb90c4ffd98d8565f360e21a5
Sergey Muratov <van.der.msa@gmail.com>
2019-05-24 08:49:39 +0300

    State after Chapter 19

--- WasRun.py
+++ WasRun.py
@@ -2,8 +2,12 @@ from TestCase import TestCase
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = None
         TestCase.__init__(self, name)
+        self.wasRun = None
         
     def testMethod(self):
         self.wasRun = 1
+        
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
